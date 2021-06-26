37fc0a15eacb732aa7878b19dda2c31b5b146cd0
tuitentungne <85294774+tuitentungne@users.noreply.github.com>
2021-06-26 14:54:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink:  https://www.facebook.com/MyNameSilas
+    messLink:  https://www.fb.me/MyNameSilas
 }
